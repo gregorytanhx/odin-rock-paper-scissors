2dd2de1d497b9455afc84fbee7ebd377d0e50535
gregorytanhx <48156960+gregorytanhx@users.noreply.github.com>
2025-01-01 17:40:19 +0800

    adds logic for showing moves of players

--- script.js
+++ script.js
@@ -10,76 +10,94 @@ function getComputerChoice() {
 }
 
 function gameResult(a, b) {
-    let result = "";
+    let result = '';
     let winner = 0;
     if (a == b) {
-        result = "Draw" ;
+        result = 'Draw';
         winner = -1;
     } else if (a == 'rock' && b == 'scissors') {
         result = 'Rock beats Scissors!';
-    } else if (a == 'paper' && b == 'rock') {   
+    } else if (a == 'paper' && b == 'rock') {
         result = 'Paper beats Rock!';
-    }  else if (a == 'scissors' && b == 'paper') {
+    } else if (a == 'scissors' && b == 'paper') {
         result = 'Scissors beats Paper!';
     } else {
         return new Array(gameResult(b, a)[0], 1);
     }
 
     return [result, winner];
-
 }
 
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
 
-    const buttons = document.querySelectorAll("button");
+    const buttons = document.querySelectorAll('button');
     buttons.forEach((button) => {
         // add event listener for all
-        button.addEventListener("click", () => {
+        button.addEventListener('click', () => {
             playRound(button.id, getComputerChoice());
         });
     });
 
-    
-
+    function getImage(id) {
+        if (id == 'rock') {
+            return "<img src='assets/stone.png'/>";
+        } else if (id == 'paper') {
+            return "<img src='assets/toilet-roll.png'/>";
+        } else {
+            return "<img src='assets/scissors.png'/>";
+        }
+    }
 
     function playRound(humanChoice, computerChoice) {
-        const displayResult = document.querySelector(".results");
-        const humanScoreDisplay = document.querySelector(".human .score");
-        const computerScoreDisplay = document.querySelector(".computer .score");
-        const displayWinner = document.querySelector(".winner");
+        const displayResult = document.querySelector('.results');
+        const humanScoreDisplay = document.querySelector('.human .score');
+        const computerScoreDisplay = document.querySelector('.computer .score');
+        const displayWinner = document.querySelector('.winner');
+
+        // insert image of move played
+        humanMove = document.querySelector('.human-container .move');
+        humanMove.innerHTML = getImage(humanChoice);
+        humanDiv.parentNode.insertBefore(
+            humanMove.firstChild,
+            humanDiv.nextSibling
+        );
+
+        computerMove = document.querySelector('.computer-container .move');
+        computerMove.innerHTML = getImage(computerChoice);
+        computerDiv.parentNode.insertBefore(
+            humanMove.firstChild,
+            computerDiv.nextSibling
+        );
 
         let result = gameResult(humanChoice, computerChoice);
 
         if (result[1] == 0) {
-            displayResult.textContent = "You win! " + result[0];
+            displayResult.textContent = 'You win! ' + result[0];
             humanScore++;
         } else if (result[1] == 1) {
-            displayResult.textContent = "You lose! " + result[0];
+            displayResult.textContent = 'You lose! ' + result[0];
             computerScore++;
         } else {
             displayResult.textContent = result[0];
         }
-        humanScoreDisplay.textContent = "Human: " + humanScore;
-        computerScoreDisplay.textContent = " Computer: " + computerScore;
-        
+        humanScoreDisplay.textContent = 'Human: ' + humanScore;
+        computerScoreDisplay.textContent = ' Computer: ' + computerScore;
+
         //TODO: add popup to play again
         if (humanScore == 5 || computerScore == 5) {
             if (humanScore == 5) {
-                displayWinner.textContent = "You won!";
+                displayWinner.textContent = 'You won!';
             } else {
-                displayWinner.textContent = "Computer won!";
+                displayWinner.textContent = 'Computer won!';
             }
             humanScore = 0;
             computerScore = 0;
         } else {
-            displayWinner.textContent = "";
+            displayWinner.textContent = '';
         }
     }
-
-
 }
 
 playGame();
-  
